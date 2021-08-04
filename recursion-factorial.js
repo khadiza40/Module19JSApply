bc4c2cd653e1774e973cb37c6cb6238f3b4ad076
BK -40 <bkhadiza277@gmail.com>
2021-08-04 18:25:32 +0600

    recursion

--- recursion-factorial.js
+++ recursion-factorial.js
@@ -5,5 +5,11 @@ function factorial(n){
     }
      return n* factorial(n-1);
 }
-const getFactorial = factorial(7);
-console.log(getFactorial);
+const number1 = 4;
+const number2 = 6;
+const number3 = 8;
+const number4 = 10;
+console.log("4! factorial is  ",factorial(number1),"\n");
+console.log("6! factorial is  ",factorial(number2) ,"\n");
+console.log("8! factorial is  ",factorial(number3),"\n");
+console.log("10! factorial is  ",factorial(number4),"\n");
